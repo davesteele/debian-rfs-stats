5c1fe512d6c72128c87d92b34bf03d770fcc5801
David Steele <dsteele@gmail.com>
2018-01-09 17:03:11 +0000

    Switch from os.system to subprocess to catch OOM

--- bin/rfsfetch.py
+++ bin/rfsfetch.py
@@ -4,14 +4,16 @@
 import urllib
 import os
 import gzip
+import subprocess
 
 
 PKG_URL="http://http.us.debian.org/debian/dists/%s/main/binary-%s/Packages.xz"
 
 BUG_URL="http://bugs.debian.org/cgi-bin/bugreport.cgi?bug="
 
-def get_cache_object( filename, url, uni=True ):
+TMP_DIR=os.getcwd() + "/tmp"
 
+def get_cache_object( filename, url, uni=True ):
     if not os.access(filename, os.R_OK):
         fname, hdrs = urllib.urlretrieve(url, filename)
 
@@ -24,15 +26,16 @@ def get_cache_object( filename, url, uni=True ):
         return text
 
 def get_rfs_text(rfsnum):
-    return(get_cache_object('tmp/rfs%s.cache' % rfsnum, BUG_URL+rfsnum))
+    return(get_cache_object('%s/rfs%s.cache' % (TMP_DIR, rfsnum), BUG_URL+rfsnum))
 
 def get_package_text(distname, arch):
     url = PKG_URL % (distname, arch)
 
-    cache_file = "tmp/Packages-%s-%s.xz" % (distname, arch)
+    cache_file = "%s/Packages-%s-%s.xz" % (TMP_DIR, distname, arch)
     get_cache_object(cache_file, url, False)
 
-    os.system("unxz %s" % cache_file)
+#    os.system("unxz %s" % cache_file)
+    subprocess.call(['unxz', cache_file])
     f = open(cache_file[:-3])
     text = f.read()
     f.close()
